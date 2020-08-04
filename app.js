8ec5bf692805dfad434ef3252944e3c5f22f27b8
Tadas Talaikis <talaikis.tadas@gmail.com>
2020-08-04 11:55:47 +0300

    initial

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-require('http').createServer((req, res) => {
-  res.end('Hello World\n')
-}).listen(process.env.PORT)
+const http = require('http')
+const PORT = 3000
+
+http.createServer((req, res) => {
+  res.end('Hello World from Node.js\n')
+}).listen(PORT)
